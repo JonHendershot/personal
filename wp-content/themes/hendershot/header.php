1996a348768283c734afe8fccad1ac0ebe4e6cfd
Jonathan Hendershot <jonhendershot@MacBook-Pro.local>
2017-02-06 21:26:45 -0500

    adding some header meta

--- wp-content/themes/hendershot/header.php
+++ wp-content/themes/hendershot/header.php
@@ -8,12 +8,15 @@
  *
  * @package hendershot
  */
+ 
+ $image_url = get_the_post_thumbnail_url($post->ID, 'large');
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="<?php echo $image_url; // since we don't use a lot of in-line images, lets establish which image gets used for a page previewer?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
@@ -21,8 +24,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<?php $image_url = get_the_post_thumbnail_url($post->ID, 'large'); ?>
-	<img src="<?php echo $image_url; ?>" class="index-image"/>
 	<div id="content" class="site-content">
 		<?php wp_nav_menu(array('theme_location' => 'Primary', 'menu' => 'Main Menu')); ?>
 		<div class="mobile-menu-trigger header" data-menu="menu-main-menu-container">Menu</div>
