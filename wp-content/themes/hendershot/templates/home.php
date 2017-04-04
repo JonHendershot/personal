3c3724c01803b98975d09f6d2e0f9e95c13bc873
Jonathan Hendershot <jonhendershot@MacBook-Pro.fios-router.home>
2017-04-04 18:47:26 -0400

    change UX/UI order on home page fader

--- wp-content/themes/hendershot/templates/home.php
+++ wp-content/themes/hendershot/templates/home.php
@@ -12,9 +12,9 @@
 			<h1>Jon Hendershot</h1>
 			<div class="creative-titles">
 				<p class="sub-title visible slide-1" data-id="1">Full-Stack Developer</p>
-				<p class="sub-title slide-2" data-id="2">UX Designer</p>
+				<p class="sub-title slide-2" data-id="2">UI Designer</p>
 				<p class="sub-title slide-3" data-id="3">Brand Strategist</p>
-				<p class="sub-title slide-4" data-id="4">UI Designer</p>
+				<p class="sub-title slide-4" data-id="4">UX Designer</p>
 			</div>
 		</div>
 	</div>
